0abc273e9f1db3c87db82f1d6c7886f3dea2863c
Maldonado <joseromaldo.1997@gmail.com>
2024-08-12 09:42:13 -0600

    Modificación para que corra: .env APP_NAME se cambia el nombre al nuevo proyecto, se comentarió header('location: /') en database.php y se cambiaron las variables a $_ENV en database.php

--- views/Producto/index.php
+++ views/Producto/index.php
@@ -1,11 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Proyecto Prueba</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">FORMULARIO PARA PRODUCTOS</h1>
+        <div class="border shadow p-4">
+            <form action="">
+                <div class="row mb-3">
+                    <div class="col-md-4">
+                        <label for="nombre" class="form-label">Nombre del producto</label>
+                        <input type="text" id="nombre" class="form-control" placeholder="Ingrese el nombre del producto">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="precio" class="form-label">Ingrese precio</label>
+                        <input type="number" id="precio" name="precio" min="0" step="0.01" placeholder="0.00" id="precio" class="form-control">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="descripcion" class="form-label">Descripción del producto</label>
+                        <input type="text" id="descripcion" class="form-control" placeholder="Ingrese la descripción">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
